6e5ee45f47f0239452d6da2854c216b237d73cda
Joseceron101 <2430933@clases.edu.sv>
2024-05-14 21:08:19 -0600

    ?

--- src/App.java
+++ src/App.java
@@ -69,7 +69,26 @@ public class App {
     }
 
     private static void ejercicio2() {
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt();
 
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingresa el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        int mayor = num1;
+
+        if (num2 > mayor) {
+            mayor = num2;
+        }
+
+        if (num3 > mayor) {
+            mayor = num3;
+        }
+        System.out.println("El número mayor es " + mayor);
+    }
     }
 
     private static void ejercicio1() {
