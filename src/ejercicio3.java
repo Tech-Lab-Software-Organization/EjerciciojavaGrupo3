dc0bfd1551ae85d013f6b63681a367eccb5292a0
yeraldycarpio <2430933@gmail.com>
2024-05-15 17:16:37 -0600

    Promedio de tres notas

--- src/ejercicio3.java
+++ src/ejercicio3.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ejercicio3 {
     public static void main(String[] args) {
         
+        Scanner entrada = new Scanner(System.in);
+
+        Float nota1, nota2, nota3 ;
+        Float promedio ;
+        
+        System.out.println("Ingrese la primera nota");
+        nota1 = entrada.nextFloat();
+
+        System.out.println("Ingrese la segunda nota");
+        nota2 = entrada.nextFloat();
+
+        System.out.println("Ingrese la tercera nota");
+        nota3 = entrada.nextFloat();
+
+        promedio= (nota1 + nota2 + nota3)/3;
+
+        if (promedio>=10) {
+            System.out.println("Felicidades estas aprobado: " + promedio);
+           
+        }
+        else{
+            System.out.println("Estas reprobado:" + promedio);
+        }
+        
+
+    
     }
 }
