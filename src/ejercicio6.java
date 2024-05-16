6dce15452c7f45d917a23be27ce34c6d88d35f8e
Jose47s <renderoshernandezjosemanuel@gmail.com>
2024-05-15 18:36:38 -0600

    add: Styles, aplicando el ejercicio 2 de calcular la area de una circunferencia.

--- src/ejercicio6.java
+++ src/ejercicio6.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ejercicio6 {
     public static void main(String[] args) {
-        
-    }
+   Scanner scanner = new Scanner(System.in);
+
+   System.out.println("Ingrese el radio de la circunferencia:");
+   double radio = scanner.nextDouble();
+
+   double area = Math.PI * radio * radio;
+
+   System.out.println("El área de la circunferencia es: " + area);
+
+   scanner.close();
+}
 }
