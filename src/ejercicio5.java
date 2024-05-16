e414f2a04273911c74002cd790a45e5272b530da
Nery David Méndez Méndez <nerydavidmendezmendez@outlook.com>
2024-05-15 22:42:36 -0600

    se modifico el ejercicio 5 (Nery David Méndez Méndez)

--- src/ejercicio5.java
+++ src/ejercicio5.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int suma = 0;
+        int numero;
+
+        System.out.println("Ingrese 0 si quiere detener el programa");
+        System.out.println("Ingrese los numeros a sumar.");
+
+        do {
+            System.out.println("Ingrese un numero: ");
+            numero = scanner.nextInt();
+
+            suma += numero;
+        } while (numero != 0);
+        System.out.println("La suma de los numeros es: " + suma);
+        scanner.close();
     }
 }
