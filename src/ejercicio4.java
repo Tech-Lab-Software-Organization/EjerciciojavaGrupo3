464879eeefae9d9f029cb8b9ed5a4e12655de3f5
Yulissa22 <ec1413220@gmail.com>
2024-05-15 18:01:11 -0600

    Descuento

--- src/ejercicio4.java
+++ src/ejercicio4.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class ejercicio4 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Ingrese el precio del producto: ");
+        double precio = scanner.nextDouble();
+
+        System.out.println("Ingrese color de la etiqueta (verde, rojo, azul, dorada): ");
+        String colorEtiqueta = scanner.next().toLowerCase();
+
+        double descuento = 0;
+        switch (colorEtiqueta) {
+        case "verde":
+            descuento = precio * 0.5;
+            break;
+        case "rojo":
+            descuento = precio * 0.1;
+            break;
+        case "azul":
+            descuento = precio * 0.15;
+            break;
+        case "dorada":
+            descuento = precio * 0.20;
+            break;
+        default:
+            System.out.println("El color de la etiqueta no existe.");
+            return;
+     }
+        double montoAPagar = precio - descuento;
+
+        System.out.println("Descuento aplicado: " + descuento);
+        System.out.println("Monto a pagar: $" + montoAPagar);
+
+        scanner.close();
+
     }
 }
